8a05ceceeb454d8a41ba4fcc4023df0409a0a6af
Michal Martusewicz <michal.martusewicz@catalysts.cc>
2020-06-26 17:44:13 +0200

    Moved some function to utils Added requirement Minor fix

--- src/net.py
+++ src/net.py
@@ -5,7 +5,7 @@ from torch.nn import functional as F
 
 class Net(nn.Module):
     def __init__(self):
-        super(Net, self).__init__()
+        super().__init__()
         self.conv1 = nn.Conv2d(1, 32, 3, 1)
         self.conv2 = nn.Conv2d(32, 64, 3, 1)
         self.dropout1 = nn.Dropout2d(0.25)
@@ -29,19 +29,49 @@ class Net(nn.Module):
         return output
 
 
-def train(model, device, train_loader, optimizer, epoch):
+class ExperimentalNet(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(1, 28, 3, 1)
+        self.conv2 = nn.Conv2d(28, 56, 3, 1)
+        self.dropout1 = nn.Dropout2d(0.25)
+        self.dropout2 = nn.Dropout2d(0.5)
+        self.fc1 = nn.Linear(8064, 128)
+        self.fc2 = nn.Linear(128, 10)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = F.relu(x)
+        x = self.conv2(x)
+        x = F.relu(x)
+        x = F.max_pool2d(x, 2)
+        x = self.dropout1(x)
+        x = torch.flatten(x, 1)
+        x = self.fc1(x)
+        x = F.relu(x)
+        x = self.dropout2(x)
+        x = self.fc2(x)
+        output = F.log_softmax(x, dim=1)
+        return output
+
+
+def train(model, device, train_loader, optimizer, epoch=1, title=''):
     model.train()
-    for batch_idx, (data, target) in enumerate(train_loader):
-        data, target = data.to(device), target.to(device)
-        optimizer.zero_grad()
-        output = model(data)
-        loss = F.nll_loss(output, target)
-        loss.backward()
-        optimizer.step()
-        if batch_idx % 100 == 0:
-            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
-                epoch, batch_idx * len(data), len(train_loader.dataset),
-                       100. * batch_idx / len(train_loader), loss.item()))
+    if title != '':
+        print(title)
+    for i in range(epoch):
+        for batch_idx, (data, target) in enumerate(train_loader):
+            data, target = data.to(device), target.to(device)
+
+            optimizer.zero_grad()
+            output = model(data)
+            loss = F.nll_loss(output, target)
+            loss.backward()
+            optimizer.step()
+            if batch_idx % 100 == 0:
+                print('Train, Epoch: {}/{} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                    i + 1, epoch, batch_idx * len(data), len(train_loader.dataset),
+                    100. * batch_idx / len(train_loader), loss.item()))
 
 
 def test(model, device, test_loader):
--- src/util.py
+++ src/util.py
@@ -1,8 +1,11 @@
 import json
 import os
 
+import matplotlib.pyplot as plt
 import numpy as np
 from PIL import Image
+from sklearn.manifold import TSNE
+from tqdm import tqdm
 
 
 def to_gray(img_path):
@@ -49,9 +52,9 @@ def check_sudoku(sudoku):
         for j in range(3):
             tmp_arr[3 * i + j] = sudoku[3 * i:3 * i + 3, 3 * j:3 * j + 3].ravel()
     return (
-        np.all(np.sort(tmp_arr, axis=1) == xx)
-        and np.all(np.sort(sudoku, axis=1) == xx)
-        and np.all(np.sort(sudoku, axis=0) == yy)
+            np.all(np.sort(tmp_arr, axis=1) == xx)
+            and np.all(np.sort(sudoku, axis=1) == xx)
+            and np.all(np.sort(sudoku, axis=0) == yy)
     )
 
 
@@ -79,3 +82,52 @@ def get_gray_images(pics_path):
 
     gray_images = np.array(gray_images)
     return gray_images, labels
+
+
+def get_pure_data(dataloader):
+    digits, labels = [], []
+    for _, (data, targets) in tqdm(enumerate(dataloader)):
+        digits.append(data.cpu().numpy().reshape(-1, 28, 28))
+        labels.append(targets.cpu().numpy())
+
+    return np.vstack(digits).reshape(-1, 28 * 28), np.hstack(labels)
+
+
+def plot_digits(X, y):
+    X = (X - X.min(axis=0)) / (X.max(axis=0) - X.min(axis=0))
+
+    plt.figure(figsize=(16, 9))
+    for i in range(X.shape[0]):
+        plt.text(X[i, 0], X[i, 1], str(y[i]), color=plt.cm.Set1(0.1 * y[i]), fontdict={'weight': 'bold', 'size': 8})
+
+    plt.xticks([])
+    plt.yticks([])
+    plt.show()
+
+
+def plot_samples(X, y, n=8):
+    plt.figure(figsize=(20, 6))
+    ids = np.random.choice(X.shape[0], n, replace=False)
+    for i in range(n):
+        plt.subplot(1, n, i + 1)
+        plt.imshow(X[ids[i]].reshape(28, 28), cmap='gray')
+        plt.title(y[ids[i]])
+        plt.xticks([])
+        plt.yticks([])
+    plt.show()
+
+
+def present_dataset(dataloader):
+    digits, labels = get_pure_data(dataloader)
+
+    plot_samples(digits, labels)
+    tsne_size = 3000
+    if digits.shape[0] > tsne_size:
+        ids = np.random.choice(digits.shape[0], tsne_size, replace=False)
+    else:
+        ids = np.ones(digits.shape[0])
+
+    transformation = TSNE(n_components=2, init='pca')
+    X = transformation.fit_transform(digits[ids].reshape(tsne_size, -1))
+
+    plot_digits(X, labels[ids])
